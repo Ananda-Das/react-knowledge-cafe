3393fc0fd4f5e784d2a99ae37aab2e629a7e504f
Ananda Das <65402344+Ananda1405@users.noreply.github.com>
2023-09-12 11:24:07 +0600

    Display Blog data and hashtags

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,16 +1,36 @@
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
-const Blog = (blog) => {
-    console.log(blog);
-    return (
-        <div>
-
+const Blog = ({ blog }) => {
+  const { title, cover, reading_time, author, author_img, posted_date, hashtags } = blog;
+  return (
+    <div>
+      <img src={cover} alt={`Cover picture of the blog ${title}`} />
+      <div className="flex justify-between">
+        <div className="flex">
+          <img className="w-14" src={author_img} alt="" />
+          <div className="ml-6">
+            <h3 className="text-2xl">{author}</h3>
+            <p>{posted_date}</p>
+          </div>
         </div>
-    );
+        <div>
+          <span>{reading_time} min read</span>
+        </div>
+      </div>
+      <h2 className="text-4xl">{title}</h2>
+      <p>
+        {hashtags.map((hash, idx) => (
+          <span key={idx}>
+            <a href="#{hash}"></a>
+          </span>
+        ))}
+      </p>
+    </div>
+  );
 };
 
 Blog.propTypes = {
-    blog: PropTypes.object
-}
+  blog: PropTypes.object.isRequired,
+};
 
 export default Blog;
--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -1,21 +1,24 @@
 import { useEffect } from "react";
 import { useState } from "react";
+import Blog from "../Blog/Blog";
 
 const Blogs = () => {
+  const [blogs, setblogs] = useState([]);
 
-    const [blogs, setblogs] = useState([]);
+  useEffect(() => {
+    fetch("blogs.json")
+      .then((res) => res.json())
+      .then((data) => setblogs(data));
+  }, []);
 
-    useEffect(() => {
-        fetch('blogs.json')
-        .then(res => res.json())
-        .then(data=> setblogs(data))
-    },[]);
-
-    return (
-        <div className="md:w-2/3">
-            <h1 className="text-4xl">Blogs: {blogs.length}</h1>
-        </div>
-    );
+  return (
+    <div className="md:w-2/3">
+      <h1 className="text-4xl">Blogs: {blogs.length}</h1>
+      {blogs.map(blog => 
+        <Blog key={blog.id} blog={blog}></Blog>
+      )}
+    </div>
+  );
 };
 
 export default Blogs;
