01325cf6fc35d147ede70ebe390a9c9e6d03948d
Ananda Das <65402344+Ananda1405@users.noreply.github.com>
2023-09-12 10:03:36 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-6xl bg-gray-200 text-center'>React Knowledge Cafe</h1>
+    </>
+  )
+}
+
+export default App
